41a103b124f8f3481e23ea45177301337b865101
jason <jasonhellwig@gmail.com>
2018-09-19 13:42:25 -0700

    added TODO for deck service.

--- api/src/services/deck/deck.service.js
+++ api/src/services/deck/deck.service.js
@@ -10,6 +10,9 @@ module.exports = function (app) {
   // Initialize our service with any options it requires
   app.use('/deck', createService(options));
 
+  //TODO extend base mongo service and add ability to create mutliples of a 52 card deck
+  //TODO add hook to validate deck creation options
+
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('deck');
 
