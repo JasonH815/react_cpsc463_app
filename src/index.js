c4efec07f809ecbb75d589a6330e9af1c86c425d
jason <jasonhellwig@gmail.com>
2018-09-18 21:37:05 -0700

    Added redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from './reducers/index'
+import thunkMiddleware from 'redux-thunk'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * Function to apply and custom or third part middleware and enhancers.
+ * Middleware is used to apply modifications to actions returned by action-creators
+ * Enhancers are decorators functions that wrap calls to reducers
+ * @param preloadedState
+ * @return {Store<any> & *}
+ */
+function configureStore(preloadedState) {
+  const middleware = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middleware);
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = compose(...enhancers);
+
+  return createStore(rootReducer, preloadedState, composedEnhancers)
+}
+
+// call our store creator
+const store = configureStore();
+
+// render the app wrapped with the store provider
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
