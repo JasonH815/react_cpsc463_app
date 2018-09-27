1e00c2b554364e0bd90c9c856cc434716e008467
jason <jasonhellwig@gmail.com>
2018-09-26 21:23:47 -0700

    Added dockerfiles for deployment

---
+++ docker/api/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+
+echo ${DIR}
+
+docker build --tag jasonh815/cardcounterz-api --file Dockerfile ../../api/
+docker push jasonh815/cardcounterz-api
+#docker run --rm -ti -p 3030:3030 --name cardcounterz cardcounterz
--- docker/mongo/start-mongo.sh
+++ docker/mongo/start-mongo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ../../${DIR}
+cd ${DIR}/../../
 
 docker pull mongo:4
 docker run --rm -ti -p 27017:27017 -v $(pwd)/api/data/mongo:/data/db --name mongo mongo:4
