25fd2952184563bc63ceeae7163d1245dc32a6ec
jason <jasonhellwig@gmail.com>
2018-12-03 12:11:53 -0800

    Added check for number of decks created

--- api/src/services/decks/decks.hooks.js
+++ api/src/services/decks/decks.hooks.js
@@ -1,3 +1,22 @@
+const {BadRequest} = require('@feathersjs/errors');
+
+const MAX_DECKS = 5;
+
+function validateMaxDecks(context) {
+  function checkMaxDecks(n){
+    if (Number.isNaN(n) || parseInt(n) > MAX_DECKS) {
+      throw new BadRequest(`Must create no more than ${MAX_DECKS} at a time`);
+    }
+  }
+
+  if (Array.isArray(context.data)) {
+    context.data.forEach(data => checkMaxDecks(data.count));
+  } else {
+    checkMaxDecks(context.data.count);
+  }
+  return context;
+}
+
 
 
 module.exports = {
@@ -5,7 +24,7 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [],
+    create: [validateMaxDecks],
     update: [],
     patch: [],
     remove: []
